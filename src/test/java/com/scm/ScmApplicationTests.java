c20a0245433ac3284748d086a4564627dce0dc82
sagnikbose1998 <reachtosagnikbose@gmail.com>
2024-10-22 07:16:10 +0530

    Env config

--- src/test/java/com/scm/ScmApplicationTests.java
+++ src/test/java/com/scm/ScmApplicationTests.java
@@ -1,26 +1,26 @@
-package com.scm;
-
-import com.scm.services.EmailService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ScmApplicationTests {
-
-	@Autowired
-	private EmailService emailService;
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void sendEmails(){
-		emailService.sendEmail(
-				"reachtosagnikbose@gmail.com",
-				"Testing for mailtrap mails",
-				"Testing body for baatcheet"
-		);
-	}
-
-}
+//package com.scm;
+//
+//import com.scm.services.EmailService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ScmApplicationTests {
+//
+//	@Autowired
+//	private EmailService emailService;
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//	@Test
+//	void sendEmails(){
+//		emailService.sendEmail(
+//				"reachtosagnikbose@gmail.com",
+//				"Testing for mailtrap mails",
+//				"Testing body for baatcheet"
+//		);
+//	}
+//
+//}
